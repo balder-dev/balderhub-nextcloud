634ccafabc44e8b0acb879fc93a2ff40483c794f
matosys <max@matosys.de>
2026-04-28 19:34:35 +0200

    feat(BasePage): implement MenuSetting object for main menu on right top of web application

--- src/balderhub/nextcloud/lib/pages/web/base_page.py
+++ src/balderhub/nextcloud/lib/pages/web/base_page.py
@@ -3,10 +3,11 @@ from typing import Union, List
 import balder
 from balderhub.html.lib.scenario_features.html_page import HtmlPage
 from balderhub.html.lib.utils.selector import Selector
+import balderhub.html.lib.utils.components as html
 from balderhub.url.lib.utils import Url
 
 from balderhub.nextcloud.lib.scenario_features import IsNextcloudServer
-from balderhub.nextcloud.lib.utils.components.web import ModalVideo, ModalWelcome
+from balderhub.nextcloud.lib.utils.components.web import ModalVideo, ModalWelcome, SettingMenu
 
 
 class BasePage(HtmlPage):
@@ -43,3 +44,33 @@ class BasePage(HtmlPage):
             Selector.by_xpath(".//div[contains(@class, 'modal-wrapper') "
                               "and .//h2[contains(text(), 'A collaboration platform that puts you in control')]]")
         )
+
+    @property
+    def _btn_setting_menu(self):
+        return html.HtmlButtonElement.by_selector(
+            self.driver,
+            Selector.by_xpath('.//button[@aria-label="Settings menu"]')
+        )
+
+    def open_setting_menu(self, timeout_sec: float = 1) -> SettingMenu:
+        """
+        Open the settings menu.
+
+        This method attempts to locate and return the `SettingMenu` instance associated
+        with the application's user interface. If the settings menu is already visible,
+        it retrieves it directly. Otherwise, it interacts with the UI to display the
+        settings menu and waits for it to become visible within a specific timeout period.
+
+        :param timeout_sec: timeout in seconds
+        :return: An instance of `SettingMenu` representing the settings menu.
+        :raises TimeoutException: If the settings menu does not become visible within
+            the specified timeout period.
+        """
+        setting_menu = SettingMenu.by_selector(
+            self.driver,
+            Selector.by_id('header-menu-user-menu')
+        )
+        if setting_menu.is_visible():
+            return setting_menu
+        self._btn_setting_menu.click()
+        return setting_menu.wait_to_be_visible_for(timeout_sec)
