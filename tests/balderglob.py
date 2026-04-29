6defcfe3d77623b9746cb86163ef2fd68ba3e038
matosys <max@matosys.de>
2026-04-29 11:45:28 +0200

    tests(balderglob.py): enable logging

--- tests/balderglob.py
+++ tests/balderglob.py
@@ -1,1 +1,7 @@
+import logging
 from balderplugin.junit import JunitPlugin
+
+
+logging.basicConfig(format='[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s', level=logging.DEBUG)
+logging.getLogger('selenium').setLevel(logging.INFO)
+logging.getLogger('urllib3').setLevel(logging.INFO)
